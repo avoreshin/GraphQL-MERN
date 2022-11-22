c5979bb371a2966d6551dd028967ec1fa6de9c5a
avoreshin <avoreshin@gmail.com>
2022-11-23 00:51:35 +0300

    feat: graphql

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const color = require('colors')
 require('dotenv').config();
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema/schema')
 const port = process.env.PORT || 3000;
-
+const connectDB = require('./config/db')
 const app = express();
+
+connectDB();
 
 app.use('/graphql', graphqlHTTP({
     schema,
